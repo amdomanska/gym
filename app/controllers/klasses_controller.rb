923673d33a8acbb4f8ab165002c8cc63f81a7800
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2017-12-15 20:08:38 +0000

    Adding new class to db by Submit btn in a form; hirb installed

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -2,4 +2,19 @@ class KlassesController < ApplicationController
 	def new
     	@klass = Klass.new
 	end
+	
+	def create
+		@klass = Klass.new(contact_params)
+		if @klass.save
+			flash[:success] = "Class added"
+		else
+			flash[:error] = @klass.errors.full_messages.join(", ")
+		end
+		redirect_to new_klass_path
+	end
+	
+	private
+	def contact_params
+		params.require(:klass).permit(:name, :teacher, :day, :start, :duration)
+	end
 end
