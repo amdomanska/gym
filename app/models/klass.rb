74b60640126fea51a3367fc22fc1fa82cc84a29c
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2017-12-21 22:34:20 +0000

    form changed for user-friendly HH:MM format of start_time

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -1,7 +1,11 @@
 class Klass < ActiveRecord::Base
+     
+     attr_accessor(:start_hour, :start_mins)
+     
      validates :name, presence: true
      validates :teacher, presence: true
      validates :day, presence: true
-     validates :start, presence: true
      validates :duration, presence: true, numericality: { only_integer: true }
+     
+     
 end
